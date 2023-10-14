86e6c77c7fe561af49da94b5d059866470c0f3b3
MasterMilkyshake <147929118+MasterMilkyshake@users.noreply.github.com>
2023-10-14 10:06:05 -0400

    Add files via upload

---
+++ Website/website/views.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def home():
+
+    return render_template("index.html")
+
+@views.route('/leadership.html')
+def leadership():
+
+    return render_template("/leadership/index.html")
