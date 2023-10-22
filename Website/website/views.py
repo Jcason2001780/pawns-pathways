8afaa35537ed470393d59e4a7789288182d1f29b
cx18121 <charlie.l.xue@gmail.com>
2023-10-21 21:34:19 -0400

    added libraries page

--- Website/website/views.py
+++ Website/website/views.py
@@ -2,12 +2,26 @@ from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
+
 @views.route('/')
 def home():
 
     return render_template("index.html")
 
+
 @views.route('/leadership')
 def leadership():
 
     return render_template("/leadership/index.html")
+
+
+@views.route('/plainsboro')
+def plainsboro():
+
+    return render_template("/plainsboro/index.html")
+
+
+@views.route('/trenton')
+def trenton():
+
+    return render_template("/trenton/index.html")
