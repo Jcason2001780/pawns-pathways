ae534feddff52ad3d698416ce3e69f845d9591eb
Master-Milkshake <147929118+Master-Milkshake@users.noreply.github.com>
2023-10-24 17:52:49 -0400

    Add files via upload

--- Website/website/views.py
+++ Website/website/views.py
@@ -8,12 +8,30 @@ def home():
 
     return render_template("index.html")
 
-
 @views.route('/leadership')
 def leadership():
 
     return render_template("/leadership/index.html")
 
+@views.route('/news')
+def news():
+
+    return render_template("/news/index.html")
+
+@views.route('/curriculum')
+def curriculum():
+
+    return render_template("/curriculum/index.html")
+
+@views.route('/classes')
+def classes():
+
+    return render_template("/classes/index.html")
+
+@views.route('/events')
+def events():
+
+    return render_template("/events/index.html")
 
 @views.route('/plainsboro')
 def plainsboro():
@@ -25,3 +43,33 @@ def plainsboro():
 def trenton():
 
     return render_template("/trenton/index.html")
+
+@views.route('/hamilton')
+def hamilton():
+
+    return render_template("/hamilton/index.html")
+
+@views.route('/princeton')
+def princeton():
+
+    return render_template("/princeton/index.html")
+
+@views.route('/franklin')
+def franklin():
+
+    return render_template("/franklin/index.html")
+
+@views.route('/cranbury')
+def cranbury():
+
+    return render_template("/cranbury/index.html")
+
+@views.route('/brunswick')
+def brunswick():
+
+    return render_template("/brunswick/index.html")
+
+@views.route('/monroe')
+def monroe():
+
+    return render_template("/monroe/index.html")
