1b2cce9c1c96ca28108003ea3ab3569d6974e5a1
Master-Milkshake <147929118+Master-Milkshake@users.noreply.github.com>
2023-10-28 00:08:49 -0400

    Update scroll.js

--- Website/website/static/scripts/scroll.js
+++ Website/website/static/scripts/scroll.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    initializeScrolling('.news-coverage');
-    initializeScrolling('.partnered-libraries');
+    initializeScrolling('.news-coverage', 6, 6, true);
+    initializeScrolling('.partnered-libraries', 8, 8, false);
 });
 
-function initializeScrolling(sectionSelector) {
+function initializeScrolling(sectionSelector, initialLeftArrowPressCount, initialRightArrowPressCount, isNewsCoverage) {
     const section = document.querySelector(sectionSelector);
+    if (!section) return;
+
     const scrollContainer = section.querySelector('.infinite-scroll-container');
     const scrollWrapper = section.querySelector('.infinite-scroll-wrapper');
     const leftArrow = section.querySelector('.arrow.left');
@@ -13,8 +15,9 @@ function initializeScrolling(sectionSelector) {
     const middlePosition = (scrollContainer.scrollWidth - scrollWrapper.clientWidth) / 2;
     scrollWrapper.scrollLeft = middlePosition;
 
-    let leftArrowPressCount = 6;
-    let rightArrowPressCount = 6;
+    let leftArrowPressCount = initialLeftArrowPressCount;
+    let rightArrowPressCount = initialRightArrowPressCount;
+    let isAnimating = false;
 
     function updateArrowPositions() {
         const rect = scrollWrapper.getBoundingClientRect();
@@ -22,15 +25,37 @@ function initializeScrolling(sectionSelector) {
     }
 
     function handleArrowClick(direction) {
+        if (isAnimating) return;
+        isAnimating = true;
+
+        const maxPressCount = isNewsCoverage ? 6 : 8;
+        const resetLeftPressCount = isNewsCoverage ? 5 : 7;
+        const resetRightPressCount = isNewsCoverage ? 7 : 5;
+
         if (direction === 'left' && leftArrowPressCount > 0) {
             scrollWrapper.scrollBy({ left: -350, behavior: 'smooth' });
             leftArrowPressCount--;
-            rightArrowPressCount = Math.min(rightArrowPressCount + 2, 8 - leftArrowPressCount);
+            rightArrowPressCount = Math.min(rightArrowPressCount + 2, maxPressCount - leftArrowPressCount);
         } else if (direction === 'right' && rightArrowPressCount > 0) {
             scrollWrapper.scrollBy({ left: 350, behavior: 'smooth' });
             rightArrowPressCount--;
-            leftArrowPressCount = Math.min(leftArrowPressCount + 2, 8 - rightArrowPressCount);
+            leftArrowPressCount = Math.min(leftArrowPressCount + 2, maxPressCount - rightArrowPressCount);
+        } else if (direction === 'left') {
+            scrollWrapper.scrollLeft = middlePosition;
+            scrollWrapper.scrollBy({ left: -350, behavior: 'smooth' });
+            leftArrowPressCount = resetLeftPressCount;
+            rightArrowPressCount = resetRightPressCount;
+        } else if (direction === 'right') {
+            scrollWrapper.scrollLeft = middlePosition;
+            scrollWrapper.scrollBy({ left: 350, behavior: 'smooth' });
+            leftArrowPressCount = resetRightPressCount;
+            rightArrowPressCount = resetLeftPressCount;
         }
+
+        // Reset isAnimating flag after animation completes
+        setTimeout(() => {
+            isAnimating = false;
+        }, 400); // Adjust the timeout duration to match your scroll animation duration
     }
 
     leftArrow.addEventListener('click', () => handleArrowClick('left'));
@@ -39,12 +64,11 @@ function initializeScrolling(sectionSelector) {
     // Prevent horizontal scrolling with mouse wheel
     document.addEventListener('wheel', function(e) {
         if (e.deltaY !== 0) {
-          e.preventDefault();  // Prevent the default scroll behavior
-          window.scrollBy(0, e.deltaY * 0.5);  // Scrolling speed is half of the normal speed
+            e.preventDefault();
+            window.scrollBy(0, e.deltaY * 0.5);
         }
-    }, { passive: false });      
+    }, { passive: false });
 
-    // Update the arrow positions initially and whenever the user scrolls
     updateArrowPositions();
     document.addEventListener('scroll', updateArrowPositions);
 }
