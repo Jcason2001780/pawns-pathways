0c1cf9a49475a599d5b8f5c6ac1f38e87d102938
Abhishek Pallepati <147929118+Master-Milkshake@users.noreply.github.com>
2023-11-02 13:19:54 -0400

    Update calendar.js

--- Website/website/static/scripts/calendar.js
+++ Website/website/static/scripts/calendar.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'dayGridMonth',
-        initialDate: '2023-10-30',
         headerToolbar: {
             left: 'prev,next today',
             center: 'title',
