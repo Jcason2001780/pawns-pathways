e2a7ca7f54d2c3f1099779f09843c9e9b41315ed
cx18121 <charlie.l.xue@gmail.com>
2023-11-20 23:09:27 -0500

    finished organizing css

--- Website/website/static/scripts/reveal.js
+++ Website/website/static/scripts/reveal.js
@@ -1,23 +1,20 @@
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-    for (var i = 0; i < reveals.length; i++) {
-        var rect = reveals[i];
-        const elementIsVisibleInViewport = (el, partiallyVisible = false) => {
-            const { top, left, bottom, right } = el.getBoundingClientRect();
-            const { innerHeight, innerWidth } = window;
-            return partiallyVisible
-                ? ((top > 0 && top < innerHeight) ||
-                    (bottom > 0 && bottom < innerHeight)) &&
-                ((left > 0 && left < innerWidth) || (right > 0 && right < innerWidth))
-                : top >= 0 && left >= 0 && bottom <= innerHeight && right <= innerWidth;
-        };
-        if (elementIsVisibleInViewport(rect, true)) {
-            reveals[i].classList.add("active");
-        }
-        else {
-            reveals[i].classList.remove("active");
-        }
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var rect = reveals[i];
+    const elementIsVisibleInViewport = (el, partiallyVisible = false) => {
+      const { top, left, bottom, right } = el.getBoundingClientRect();
+      const { innerHeight, innerWidth } = window;
+      return partiallyVisible
+        ? (top > 0 && top < innerHeight) || (bottom > 0 && bottom < innerHeight)
+        : top >= 0 && bottom <= innerHeight;
+    };
+    if (elementIsVisibleInViewport(rect, true)) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
+  }
 }
 window.addEventListener("scroll", reveal);
 
